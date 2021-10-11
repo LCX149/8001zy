5e2fa660e7033c0a0bb5b8d43f0b2c6bdf85fe77
LCX149 <75821835+LCX149@users.noreply.github.com>
2021-10-11 15:38:00 +0800

    Update az.sh

--- az.sh
+++ az.sh
@@ -180,7 +180,7 @@ docker run -dit \
     --name $CONTAINER_NAME \
     --hostname $CONTAINER_NAME \
     -p 13570:5700 \
-    -m 1024M \
+    -m 1524M \
     --restart always \
     --network bridge \
     $DOCKER_IMG_NAME:$TAG
